8bf53181afee3fa2589f280d043022483c0a3be4
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-12-09 16:33:48 +0100

    progress report program on cmd line

--- 01-01/timers.js
+++ 01-01/timers.js
@@ -1,11 +1,15 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitIntercval = 500;
 let currentTime = 0; 
 
 
 const incTime = () => {
     currentTime += waitIntercval;
-    console.log(`waiting ${currentTime/1000} seconds`);
+    const p = Math.floor((currentTime/waitTime)*100);
+    // console.log(`waiting ${currentTime/1000} seconds`);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting ... ${p}%`);
 };
 
 console.log(`setting a ${waitTime/1000} second delay`);
@@ -15,6 +19,8 @@ console.log(`setting a ${waitTime/1000} second delay`);
 const timer = setInterval(incTime,waitIntercval);
 const timerFinished = ()=> {
     clearInterval(timer);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
     console.log("Done ~");
 }
 
