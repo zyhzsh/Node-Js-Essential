86d3b15cf62f0c0adbf1a6b2e57f71ac095e27f5
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-12-09 16:26:23 +0100

    set the interval

--- 01-01/timers.js
+++ 01-01/timers.js
@@ -1,7 +1,22 @@
 const waitTime = 3000;
+const waitIntercval = 500;
+let currentTime = 0; 
+
+
+const incTime = () => {
+    currentTime += waitIntercval;
+    console.log(`waiting ${currentTime/1000} seconds`);
+};
+
 console.log(`setting a ${waitTime/1000} second delay`);
 
-const timerFinished = ()=> console.log("Done ~");
+
+
+const timer = setInterval(incTime,waitIntercval);
+const timerFinished = ()=> {
+    clearInterval(timer);
+    console.log("Done ~");
+}
 
 setTimeout(timerFinished,waitTime);
 
