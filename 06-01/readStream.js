99f15bea4143f2512dfd83d3261462fd7c45f331
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-12-11 16:08:36 +0100

    Update readStream.js

--- 06-01/readStream.js
+++ 06-01/readStream.js
@@ -1,4 +1,22 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md","utf-8");
+
+
 console.log("type something...");
-process.stdin.on("data", data => {
+
+let txt="";
+//process.stdin is a readable streams
+readStream.on("data", data => {
   console.log(`I read ${data.length - 1} characters of text`);
+  txt+=data;
+});
+
+// readStream.once("data",data=>{
+//   console.log(data.length);
+// });
+
+
+readStream.on("end",()=>{
+    console.log(`finished reading file ${txt.length}`);
 });
